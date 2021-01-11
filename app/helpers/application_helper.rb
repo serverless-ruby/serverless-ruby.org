18d2aaae0acb6dd28324c713941c055e4bcecb99
Tomas Valent <equivalent@eq8.eu>
2021-01-11 23:23:04 +0100

    turbo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  # stolen from https://github.com/hotwired/turbo-rails/blob/main/app/helpers/turbo/frames_helper.rb#L23
+  def turbo_frame_tag(id, src: nil, target: nil, **attributes, &block)
+    id = id.respond_to?(:to_key) ? dom_id(id) : id
+
+    tag.turbo_frame(**attributes.merge(id: id, src: src, target: target).compact, &block)
+  end
 end
