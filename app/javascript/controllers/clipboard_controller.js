b5f28210ec1a3ff248974898d946c3ac6301317b
Tomas Valent <equivalent@eq8.eu>
2021-01-13 22:38:07 +0100

    typo fixes

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -3,8 +3,10 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [ "source" ]
   copy() {
-    alert('hhhh');
     this.sourceTarget.select()
     document.execCommand("copy")
+
+    let popover = new bootstrap.Popover(this.sourceTarget, {placement: 'bottom'})
+    popover.show()
   }
 }
